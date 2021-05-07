3b272320b872b143d4d9b5d8875c15bd017303fa
Lucijan <lucijan345@gmail.com>
2021-05-07 13:26:23 +0200

    add reset btn option with dynamic grid

--- app.js
+++ app.js
@@ -1,39 +1,71 @@
 // select all color buttons
 const btnsColors = document.querySelectorAll(".btn-color");
 
+// select container for boxes
+const container = document.querySelector(".container")
+
+// used color
 let usedColor = "black"
 
-// add color background to each button corresponding to id
+// add color backgrounds to btns and update hover listener for boxes for new color
 btnsColors.forEach(button => {
     button.style.backgroundColor = button.id
     // change color when clicked
     button.addEventListener("click", () => {
         usedColor = button.id
+        // update hover listener for boxes
+        hoverListener(boxes, usedColor)
     })
 })
-
-// select container fo boxes
-const container = document.querySelector(".container")
 
 // generate boxes
-for (let i=0; i<800; i++) {
-    let box = document.createElement("div")
-    box.classList.add("box")
-    container.appendChild(box)
-}
+addAllBtns(800)
 
 // select boxes
-const boxes = document.querySelectorAll(".box")
+let boxes = document.querySelectorAll(".box")
 
-// hover event listener
-boxes.forEach(b => {
-    b.addEventListener("mouseover", () => {
-        b.style.backgroundColor = usedColor
-    })
-})
-
-// reset button
+// select reset button
 const resetBtn = document.querySelector(".btn-reset")
+
+// event listener for reset btn
 resetBtn.addEventListener("click", () => {
-    boxes.forEach(b => {b.style.backgroundColor = "white"})
+    // get information about wanted rows and cols
+    let rows = window.prompt("Please enter number of rows.")
+    let columns = window.prompt("Please enter number of columns.")
+    // update grid templates
+    container.setAttribute("style", `width: 80%; height: 80%; background-color: #ffffff; border: 3px dotted black; display: grid; grid-template-columns: repeat(${columns}, 1fr); grid-template-rows: repeat(${rows}, 1fr);`)
+    // remove old btns
+    removeAllBtns()
+    // generate new btns
+    let numberOfBtns = rows*columns
+    addAllBtns(numberOfBtns)
+    // update boxes variable
+    boxes = document.querySelectorAll(".box")
 })
+
+// function for removing all btns
+function removeAllBtns () {
+    let boxes = document.querySelectorAll(".box")
+    boxes.forEach(b => {
+        b.remove()
+    })
+}
+
+// function for generating new btns 
+function addAllBtns(number) {
+    for (let i=0; i<(number); i++) {
+        let box = document.createElement("div")
+        box.classList.add("box")
+        container.appendChild(box)
+    }
+}
+
+// function that adds to each element hover event listener and change bg
+function hoverListener(list, color) {
+    console.log(list)
+    list.forEach(elem => {
+        elem.addEventListener("mouseover", () => {
+            elem.style.backgroundColor = color
+        })
+    })
+}
