fd6e9baa4a3fe5fcf08cec262134079a76f48c0c
LDZoid <lucijan345@gmail.com>
2021-05-26 18:50:23 +0200

    change btn style add fill and switch mode

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ---SELECTS AND VARIABLES---
+
 // default number of boxes, rows, cols and max product
 const numberDefault = 1500
 const defaultCols = 50
@@ -10,54 +12,60 @@ const btnsColors = document.querySelectorAll(".btn-color");
 // select container for boxes
 const container = document.querySelector(".container")
 
-// used color
+// select feature btns
+const resetBtn = document.querySelector("#reset")
+const clearBtn = document.querySelector("#clear")
+
+// used color (will dynamicly change)
 let usedColor = "black"
 
-// add color backgrounds to btns and update hover listener for boxes for new color
+
+
+// ---SETUP--- (generating btns and boxes dynamicly)
+
+// color btns (generating and functionality)
 btnsColors.forEach(button => {
     button.style.backgroundColor = button.id
-    // change color when clicked
+    // change used color when clicked
     button.addEventListener("click", () => {
         usedColor = button.id
         // update hover listener for boxes
-        hoverListener(boxes, usedColor)
+        hoverListener(usedColor)
     })
 })
 
 // generate boxes
-addAllBtns(numberDefault)
+addAllBoxes(defaultRows, defaultCols)
 
-// select boxes
-let boxes = document.querySelectorAll(".box")
 
-// select reset button
-const resetBtn = document.querySelector("#reset")
 
-// event listener for reset btn
+// ---BUTTON FUNCTIONALITIES---
+
+// RESET btn
 resetBtn.addEventListener("click", () => {
     // get information about wanted rows and cols
     let rows = window.prompt(`Please enter number of rows. \nDefault-rows: ${defaultRows}\nMax-product: ${maxProduct}\nMin-rows: 1`)
-    let columns = window.prompt(`Please enter number of columns. \nDefault-cols: ${defaultCols}\nMax-product: ${maxProduct}\nMin-rows: 1`)
+    let cols = window.prompt(`Please enter number of columns. \nDefault-cols: ${defaultCols}\nMax-product: ${maxProduct}\nMin-rows: 1`)
     // update grid templates
-    container.setAttribute("style", `width: 80%; height: 80%; background-color: #ffffff; display: grid; grid-template-columns: repeat(${columns}, 1fr); grid-template-rows: repeat(${rows}, 1fr);`)
-    // remove old btns
-    removeAllBtns()
-    // generate new btns
-    let numberOfBtns = rows*columns
-    addAllBtns(numberOfBtns)
+    container.setAttribute("style", `width: 80%; height: 80%; background-color: #ffffff; display: grid; grid-template-columns: repeat(${cols}, 1fr); grid-template-rows: repeat(${rows}, 1fr);`)
+    // remove old boxes
+    removeAllBoxes()
+    // generate new boxes
+    addAllBoxes(rows, cols)
     // update boxes variable
-    boxes = document.querySelectorAll(".box")
 })
 
-// event listener for clear btn
-const clearBtn = document.querySelector("#clear")
-
+// CLEAR btn
 clearBtn.addEventListener("click", () => {
     clearGrid()
 })
 
+
+
+// ---FUNCTIONS---
+
 // function for removing all btns
-function removeAllBtns () {
+function removeAllBoxes () {
     let boxes = document.querySelectorAll(".box")
     boxes.forEach(b => {
         b.remove()
@@ -65,7 +73,8 @@ function removeAllBtns () {
 }
 
 // function for generating new btns 
-function addAllBtns(number) {
+function addAllBoxes(rows, cols) {
+    let number = rows * cols
     // set a default value if input is omitted or number is too big
     if (number <= 0 || number > maxProduct) {
         number = numberDefault
@@ -79,8 +88,9 @@ function addAllBtns(number) {
 }
 
 // function that adds to each element hover event listener and change bg
-function hoverListener(list, color) {
-    list.forEach(elem => {
+function hoverListener(color) {
+    let boxes = document.querySelectorAll(".box")
+    boxes.forEach(elem => {
         elem.addEventListener("mouseover", () => {
             elem.style.backgroundColor = color
         })
@@ -89,6 +99,7 @@ function hoverListener(list, color) {
 
 // function that clears the grid from colors
 function clearGrid() {
+    let boxes = document.querySelectorAll(".box")
     boxes.forEach(box => {
         box.style.backgroundColor = "white"
     })
